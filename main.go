4c8404ea9b204e056ec1c48e8aadd9b69e2b5bbf
Luke Jones <luke@lukeojones.com>
2023-08-17 23:32:23 +0100

    feat(basics) Add Proof of Work Calculation

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
+
+const difficulty = 16
+const maxNonce = math.MaxInt64
 
 type Blockchain struct {
 	blocks []*Block
@@ -17,6 +23,47 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(block *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, 256-difficulty)
+	return &ProofOfWork{block, target}
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		Int64ToBytes(pow.block.Timestamp),
+		pow.block.Data,
+		pow.block.PrevBlockHash,
+		Int64ToBytes(int64(nonce)),
+		Int64ToBytes(int64(difficulty)),
+	}, []byte{})
+	return data
+}
+
+func (pow *ProofOfWork) Run() (nonce int, solvedHash []byte) {
+	var hashAsInt big.Int
+	var hash [32]byte
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		hashAsInt.SetBytes(hash[:])
+
+		if hashAsInt.Cmp(pow.target) == -1 {
+			break
+		}
+		nonce++
+	}
+
+	return nonce, hash[:]
 }
 
 func (blockchain *Blockchain) AddBlock(data string) {
@@ -30,8 +77,19 @@ func NewBlockchain() *Blockchain {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          []byte{},
+		Nonce:         0,
+	}
+
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash
+	block.Nonce = nonce
 	return block
 }
 
@@ -56,4 +114,16 @@ func main() {
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
 	}
+}
+
+/*
+ * Utils
+ */
+func Int64ToBytes(i int64) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, i)
+	if err != nil {
+		fmt.Println("Failed to write int64 to buffer:", err)
+	}
+	return buf.Bytes()
 }
