0697f18b6cc3f2875140742f1364fa7d30ded540
Luke Jones <luke@lukeojones.com>
2023-08-19 21:57:23 +0100

    file(cli) Used Flag Cmd Line Builder to create CLI

--- main.go
+++ main.go
@@ -1,22 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math"
+	"os"
 	"strconv"
 )
 
 const difficulty = 16
 const maxNonce = math.MaxInt64
 
-func main() {
-	blockchain := NewBlockchain()
+type CLI struct {
+	bc *Blockchain
+}
 
-	blockchain.AddBlock("Send 50 BTC to Satoshi")
-	blockchain.AddBlock("Send 25 more BTC to Nick Szabo")
-	blockchain.AddBlock("Send 12 more BTC to Luke Jones")
+func (cli *CLI) Run() {
+	cli.validateArgs()
 
-	it := blockchain.Iterator()
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("data", "", "Block Data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.PrintUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			cli.PrintUsage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.PrintChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) PrintChain() {
+	it := cli.bc.Iterator()
 	for {
 		block := it.Next()
 		fmt.Printf("Prev: %x\n", block.PrevBlockHash)
@@ -31,4 +73,25 @@ func main() {
 			break
 		}
 	}
+}
+
+func (cli *CLI) PrintUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock - Add a block of data to the blockchain")
+	fmt.Println("  printchain - Print all the blocks of the blockchain")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.PrintUsage()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	blockchain := NewBlockchain()
+	defer blockchain.db.Close()
+
+	cli := CLI{blockchain}
+	cli.Run()
 }
