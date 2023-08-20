9e7df076d455469df1cac7ce40ce4f4ebffb2fc4
Luke Jones <luke@lukeojones.com>
2023-08-20 19:29:04 +0100

    feat(transactions) Added Coinbase Tx and Tx ID Setting

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+const blockSubsidy = 50
+
 type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
@@ -17,7 +27,35 @@ type TxOutput struct {
 	ScriptPubKey string
 }
 
-const blockSubsidy = 50
+func (tx *Transaction) SetId() {
+	var encoded bytes.Buffer
+	encoder := gob.NewEncoder(&encoded)
+
+	if err := encoder.Encode(tx); err != nil {
+		log.Panic(err)
+	}
+
+	sum256 := sha256.Sum256(encoded.Bytes())
+	tx.ID = sum256[:]
+}
+
+func NewCoinbaseTx(recipient, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coinbase Reward to: %s", recipient)
+	}
+
+	dummyTxInput := TxInput{[]byte{}, -1, data}
+	output := TxOutput{blockSubsidy, recipient}
+
+	tx := Transaction{
+		ID:      nil,
+		Inputs:  []TxInput{dummyTxInput},
+		Outputs: []TxOutput{output},
+	}
+
+	tx.SetId()
+	return &tx
+}
 
 func main() {
 	blockchain := NewBlockchain()
