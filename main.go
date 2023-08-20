b319e65d3eb4ce27428e6b01f8eb7e73381b9453
Luke Jones <luke@lukeojones.com>
2023-08-20 18:14:13 +0100

    feat(transactions) Added Input, Output and Transaction types

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+type Transaction struct {
+	ID      []byte
+	Inputs  []TxInput
+	Outputs []TxOutput
+}
+
+type TxInput struct {
+	TxID          []byte
+	TxOutputIndex int
+	ScriptSig     string
+}
+
+type TxOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+const blockSubsidy = 50
+
 func main() {
 	blockchain := NewBlockchain()
 	defer blockchain.db.Close()
